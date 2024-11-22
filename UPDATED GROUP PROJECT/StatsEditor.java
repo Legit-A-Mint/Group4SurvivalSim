1dcf3dd765396305df895f7df0a1d36ca4d03aac
Jonathan-design-ship-it <Jonathanyessir123@gmail.com>
2024-11-22 14:48:10 -0500

    ZAGHDSHD

--- UPDATED GROUP PROJECT/StatsEditor.java
+++ UPDATED GROUP PROJECT/StatsEditor.java
@@ -32,6 +32,9 @@ public class StatsEditor extends World
     LeftButton leftDifficulty;
     RightButton rightDifficulty;
     
+    LeftButton leftCoins;
+    RightButton rightCoins;
+    
     NextButton start;
     
     public StatsEditor(String playerModel)
@@ -41,20 +44,23 @@ public class StatsEditor extends World
         this.playerModel = playerModel;
         
         addObject(new Background(), 512, 288);
-        addObject(leftHealth = new LeftButton(), getWidth()/4, 100);
-        addObject(rightHealth = new RightButton(), getWidth()/4*3, 100);
+        addObject(leftHealth = new LeftButton(), getWidth()/4 - 200, 150);
+        addObject(rightHealth = new RightButton(), getWidth()/4 + 200, 150);
         
-        addObject(leftSpeed = new LeftButton(), getWidth()/4, 225);
-        addObject(rightSpeed = new RightButton(), getWidth()/4*3, 225);
+        addObject(leftSpeed = new LeftButton(), getWidth()/4*3 - 200, 150);
+        addObject(rightSpeed = new RightButton(), getWidth()/4*3 + 200, 150);
         
-        addObject(leftDifficulty = new LeftButton(), getWidth()/4, 350);
-        addObject(rightDifficulty = new RightButton(), getWidth()/4*3, 350);
+        addObject(leftDifficulty = new LeftButton(), getWidth()/4 - 200, 350);
+        addObject(rightDifficulty = new RightButton(), getWidth()/4 + 200, 350);
         
-        addObject(healthTxt = new StatsLabel(new GreenfootImage("Health: " + Integer.toString(health), 90, Color.WHITE, null)), getWidth()/2, 100);
-        addObject(speedTxt = new StatsLabel(new GreenfootImage("Speed: " + Integer.toString(speed), 90, Color.WHITE, null)), getWidth()/2, 225);
-        addObject(diffTxt = new StatsLabel(new GreenfootImage(getDifficultyText(difficulty), 90, Color.WHITE, null)), getWidth()/2, 350);
+        addObject(leftCoins = new LeftButton(), getWidth()/4*3 - 200, 350);
+        addObject(rightCoins = new RightButton(), getWidth()/4*3 + 200, 350);
         
-        addObject(start = new NextButton(0), 512, 475);
+        addObject(healthTxt = new StatsLabel(new GreenfootImage("Health: " + Integer.toString(health), 50, Color.WHITE, null)), getWidth()/4, 150);
+        addObject(speedTxt = new StatsLabel(new GreenfootImage("Speed: " + Integer.toString(speed), 50, Color.WHITE, null)), getWidth()/4*3, 150);
+        addObject(diffTxt = new StatsLabel(new GreenfootImage(getDifficultyText(difficulty), 50, Color.WHITE, null)), getWidth()/4, 350);
+        
+        addObject(start = new NextButton(0), 512, 500);
     }
     
     public void act()
@@ -63,37 +69,37 @@ public class StatsEditor extends World
         {
             if (health > 10)
                 health -= 10;
-            healthTxt.setImage(new GreenfootImage("Health: " + Integer.toString(health), 90, Color.WHITE, null));
+            healthTxt.setImage(new GreenfootImage("Health: " + Integer.toString(health), 50, Color.WHITE, null));
         }
         if (Greenfoot.mouseClicked(rightHealth)||Greenfoot.isKeyDown("Right"))
         {
             if (health < 9990)
                 health += 10;
-            healthTxt.setImage(new GreenfootImage("Health: " + Integer.toString(health), 90, Color.WHITE, null));
+            healthTxt.setImage(new GreenfootImage("Health: " + Integer.toString(health), 50, Color.WHITE, null));
         }
         if (Greenfoot.mouseClicked(leftSpeed))
         {
             if (speed > 0)
                 speed--;
-            speedTxt.setImage(new GreenfootImage("Speed: " + Integer.toString(speed), 90, Color.WHITE, null));
+            speedTxt.setImage(new GreenfootImage("Speed: " + Integer.toString(speed), 50, Color.WHITE, null));
         }
         if (Greenfoot.mouseClicked(rightSpeed))
         {
             if (speed < 20)
                 speed++;
-            speedTxt.setImage(new GreenfootImage("Speed: " + Integer.toString(speed), 90, Color.WHITE, null));
+            speedTxt.setImage(new GreenfootImage("Speed: " + Integer.toString(speed), 50, Color.WHITE, null));
         }
         if (Greenfoot.mouseClicked(leftDifficulty))
         {
             if (difficulty > 0)
                 difficulty--;
-            diffTxt.setImage(new GreenfootImage(getDifficultyText(difficulty), 90, Color.WHITE, null));
+            diffTxt.setImage(new GreenfootImage(getDifficultyText(difficulty), 50, Color.WHITE, null));
         }
         if (Greenfoot.mouseClicked(rightDifficulty))
         {
             if (difficulty < 3)
                 difficulty++;
-            diffTxt.setImage(new GreenfootImage(getDifficultyText(difficulty), 90, Color.WHITE, null));
+            diffTxt.setImage(new GreenfootImage(getDifficultyText(difficulty), 50, Color.WHITE, null));
         }
         if (Greenfoot.mouseClicked(start))
         {
