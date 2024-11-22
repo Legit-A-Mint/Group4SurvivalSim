a2fe68d3a771e16fa5933c5785d29e8f7d4abcb3
darius-ho <150280900+darius-ho@users.noreply.github.com>
2024-11-22 14:15:30 -0500

    Added the 7 subclasses required for the shop icons

---
+++ UPDATED GROUP PROJECT/raft1ShopIcon.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class raft1ShopIcon here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class raft1ShopIcon extends SuperSmoothMover
+{
+    /**
+     * Act - do whatever the raft1ShopIcon wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
